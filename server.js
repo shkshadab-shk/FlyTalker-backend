375cf3462a132d005f9d81fa04f71fd6f39e5db0
shaikh shadab <112573477+shkshadab-shk@users.noreply.github.com>
2024-09-17 17:17:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// Use CORS middleware
+app.use(cors({
+  origin: 'https://fly-talker-front.vercel.app',  // Your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'https://fly-talker-front.vercel.app/',  // React frontend URL
+    origin: 'https://fly-talker-front.vercel.app',  // Your frontend URL
     methods: ['GET', 'POST']
   }
 });
@@ -18,7 +24,7 @@ io.on('connection', (socket) => {
   console.log('New client connected');
 
   socket.on('sendMessage', (data) => {
-    io.emit('receiveMessage', data);  // Broadcast the message to all clients
+    io.emit('receiveMessage', data);
   });
 
   socket.on('disconnect', () => {
